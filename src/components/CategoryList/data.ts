3abb123859e012fd66fa332eb866a99b91284b41
Felipe Lima <felip.3lima@hotmail.com>
2020-08-30 21:06:37 -0300

    Initial commmit

---
+++ src/components/CategoryList/data.ts
@@ -0,0 +1,15 @@
+import lol from '../../images/category_lol.jpg';
+import valorant from '../../images/category_valorant.jpg';
+import csgo from '../../images/category_csgo.jpg';
+import fortnite from '../../images/category_fortnite.jpg';
+import fallguys from '../../images/category_fallguys.jpg';
+import apex from '../../images/category_apex.jpg';
+
+export default [
+  { name: 'League of Legends', source: lol, views: 51.9 },
+  { name: 'VALORANT', source: valorant, views: 100.9 },
+  { name: 'Counter-Strike: Global Offensive', source: csgo, views: 90.9 },
+  { name: 'Fortnite', source: fortnite, views: 32.9 },
+  { name: 'Fall Guys', source: fallguys, views: 10.9 },
+  { name: 'Apex Legends', source: apex, views: 57.9 },
+];
